f8c9c058ba0b7d2277aa5214cd76d14e2744c60a
Markbaro <markbarasa2021@gmail.com>
2023-05-15 19:47:56 +0300

    split file stream

---
+++ tokenize.c
@@ -0,0 +1,29 @@
+#include "simple shell.h"
+
+/**
+ * tokenize - splits string into characters
+ * @str: input string
+ * @args: stores input
+ * Return:count of input strings
+ */
+char **tokenize(char *str, int args)
+{
+	char **tokenList = malloc(sizeof(char *) * size);
+	char *token;
+	int index;
+	char *delim = " :'\n''\t'";
+
+	if (line == NULL || tokenList == NULL)
+		return (NULL);
+	for (index = 0, token = strtok(line, delim); token != NULL; index++, token = strtok(NULL, delim))
+	{
+		tokenList[index] = strdup(token);
+		if (tokenList[index] == NULL)
+		{
+			free_function(2, tokenList);
+			return (NULL);
+		}
+	}
+	tokenList[index] = token;
+	return (tokenList);
+}
