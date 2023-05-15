975db8429fa945a2e04391cf766255a3564ea354
Markbaro <markbarasa2021@gmail.com>
2023-05-15 13:55:51 +0300

    creating header file

---
+++ simple_shell.h
@@ -0,0 +1,12 @@
+#ifndef SIMPLE SHELL_H
+#define SIMPLE SHELL_H
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#endif /* SIMPLE SHELL_H */
