409f60b0d82d344498968de7f616c7b12e963bf5
Markbaro <markbarasa2021@gmail.com>
2023-05-15 19:49:31 +0300

    passing exit code signals

---
+++ signal.c
@@ -0,0 +1,12 @@
+#include "simple shell.h"
+
+/**
+  * signals - evaluates SIGINT
+  * @sig: signal
+  * Return: void
+  */
+void signals(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n$ ", 3);
+}
