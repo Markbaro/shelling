6eff4c964dca07cf67b458bc014f82f28e938550
Brian-Karimi <brianmwangik97@gmail.com>
2023-05-16 15:31:12 +0300

    Header File Update

--- simple_shell.h
+++ simple_shell.h
@@ -10,7 +10,20 @@
 #include <errno.h>
 #include <signal.h>
 
+typedef struct simple_shell
+{
+    char *id;
+    int (*cmd_function)(void);
+} system_built;
+
+extern char **shell_envt;
+
 int shell_execute(char **argums);
 void signals(int sig);
-
+int arg_search(char **argums);
+char **tokenize(char *str, int args);
+int argums_counter(char *input_str);
+void mod_equals(char **str);
+int shell_system_search(char **argums);
+int directory_search(char **input_cmd, char **argums);
 #endif /* SIMPLE SHELL_H */
