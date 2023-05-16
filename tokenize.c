2bdee4f6d16d980dfb3035b278617425d4d57982
Brian-Karimi <brianmwangik97@gmail.com>
2023-05-16 15:32:13 +0300

    Parse Strings into Tokens

--- tokenize.c
+++ tokenize.c
@@ -28,3 +28,56 @@ char **tokenize(char *str, int args)
 	tokenList[index] = token;
 	return (tokenList);
 }
+/**
+ * argums_counter - counts rgums in the input string from getline
+ * @input_str:point to the input string
+ * Return: length of input_str
+*/
+
+int argums_counter(char *input_str)
+{
+	int iterate, j, flag, i = 0;
+	char *delimiter = " :";
+
+	flag  = i;
+	iterate = 1;
+
+	if (input_str == NULL)
+	return (iterate);
+
+	for (; input_str[i] != '\0'; i++)
+	{
+		while (delimiter[j] != '\0')
+		{
+			if (input_str[i] == delimiter[j] && flag == 0)
+			{
+				iterate++;
+				flag = 1;
+				break;
+			}
+		}
+		if (delimiter[j] == '\0')
+		{
+			flag = 0;
+		}
+		i++;
+	}
+	return (iterate + 1);
+}
+
+/**
+ * mod_equals - elimitates chars up including = in the envt str
+ * @str: string to edit
+ * Return: void
+*/
+void mod_equals(char **str)
+{
+	int iter = 0;
+
+	for (; str[0][iter] != '=', iter++)
+	{
+		str[0][iter] = ':';
+	}
+	str[0][iter];
+
+}
