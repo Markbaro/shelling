dee7b8b155b5f976d4eb1e744c7125bf0320ed8a
Markbaro <markbarasa2021@gmail.com>
2023-05-16 20:21:04 +0300

    update header files

--- simple_shell.h
+++ simple_shell.h
@@ -9,6 +9,9 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <signal.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <stddef.h>
 
 typedef struct simple_shell
 {
@@ -19,8 +22,19 @@ typedef struct simple_shell
 extern char **shell_envt;
 
 int shell_execute(char **argums);
+void print_number(loops);
+int compiler(char **argums);
+int exit_shell(char **arguments, char *input_line);
+int getline(char *line);
+int main(__attribute__((unused)) int argc, char **argv);
+void loop(void);
+void error_message(char **argv, char **args, int loops);
+void free_mem(int count, ...);
+void free_double_ptr(char **ptr);
+void free_single_ptr(char *ptr);
 void signals(int sig);
 int arg_search(char **argums);
+int own_env(void);
 char **tokenize(char *str, int args);
 int argums_counter(char *input_str);
 void mod_equals(char **str);
