845c159ba4f9558301cb4eca22cc91bd02692f77
Brian-Karimi <brianmwangik97@gmail.com>
2023-05-16 15:31:42 +0300

    Command Compiler and Intepretor Update

--- executor.c
+++ executor.c
@@ -1,22 +1,22 @@
 #include "simple_shell.h"
 
 /**
- * compiler -
- * @ags
+ * compiler - inteprets arguments (argums[0]) and
+ * checks the function to send to
+ * @argums: argumesnt to be passed to the function
  * Return: 0 on success
  */
-int compiler(char **ags)
+int compiler(char **argums)
 {
-	struct stat store;
+	struct stat fileStats;
 
-	return ((mySearchBuiltins(ags) == 0))
+	return (((shell_system_search(argums) == 0))
 			? 0
-			: (mySearch(ags) == -1)
+			: (arg_search(argums) == -1)
 				? -1
-				: (stat(ags[0], &store) == -1 || S_ISREG(store.st_mode) == 0)
+				: (stat(argums[0], &fileStats) == -1 || S_ISREG(fileStats.st_mode) == 0)
 					? -1
-					: (shell_execute(ags) == -1)
+					: (shell_execute(argums) == -1)
 						? -1
-						: 0;
+						: 0);
 }
-
